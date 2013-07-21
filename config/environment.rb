879eb3889c23e4bc27e9f9dc0fa551668a6b8960
Mary Ashton Inglis <maryashton.inglis@gmail.com>
2013-07-21 17:08:19 -0400

    Configured database and environment settings

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,6 @@
+ENV['GEM_PATH'] = '/home/adamjcas/ruby/gems:/usr/lib/ruby/gems/1.8'
+$:.push("/home/adamjcas/ruby/gems")
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
