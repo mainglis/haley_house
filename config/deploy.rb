0a93bd7e584741271e9f23f9268bce41b0731753
Lucas Braun <lucasalan@gmail.com>
2013-08-27 17:08:14 -0400

    Fixes for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,13 +24,13 @@ set :use_sudo, false
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
 
 
 ### TODO!!! KEEP GOING FROM HERE: https://github.com/capistrano/capistrano/wiki/2.x-From-The-Beginning#deployment-directory-structure
