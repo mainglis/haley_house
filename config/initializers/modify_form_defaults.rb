c1b690af80ca95af30a9bb8ab45ed07db070b6be
Mary Ashton Inglis <maryashton.inglis@gmail.com>
2013-12-08 10:26:58 -0500

    Format grantmaker index with sidenav bar and attempt to modify default ruby textfield size.

---
+++ config/initializers/modify_form_defaults.rb
@@ -0,0 +1,6 @@
+# All default field options are stored in one hash. It defaults to the following:
+# action_view/helpers/form_helper
+DEFAULT_FIELD_OPTIONS = { "size" => 30 }
+
+# You can delete "size" from it in an initializer, for example.
+# ActionView::Helpers::InstanceTag::DEFAULT_FIELD_OPTIONS.delete("size") 
