b813c40d8da174ce8ed1f77640b666665643f9ca
Lucas Braun <lucasalan@gmail.com>
2014-03-25 09:02:15 -0400

    Removed zipcode validation for now

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,8 +1,9 @@
 class Organization < ActiveRecord::Base
   validates :name, :presence => true
-  validates :zip, :length => { :is => 5 }
+  # validates :zip, :length => { :is => 5 }
 
-  attr_accessible :city, :mission, :name, :program, :state, :street_address, :website, :zip, :contact_phone_number, :contact_email, :contact_first_name, :contact_last_name
+  attr_accessible :city, :mission, :name, :program, :state, :street_address, :website, :zip,
+    :contact_phone_number, :contact_email, :contact_first_name, :contact_last_name
 
   def to_s
     self.name
