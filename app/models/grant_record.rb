5249c826cdc22caf61d6c724834995ef0aeafed2
Lucas Braun <lucas.braun@annkissam.com>
2014-06-25 09:12:42 -0400

    Made organization field not required for grant records, improved new grant record view

--- app/models/grant_record.rb
+++ app/models/grant_record.rb
@@ -1,11 +1,12 @@
 class GrantRecord < ActiveRecord::Base
-  validates :grantmaker_id, :organization_id, :amount, :presence => true
+
+  attr_accessible :amount, :ask_status, :fiscal_year, :funding_type, :grantmaker_id, :notes, :notification_date, :organization_id, :program, :request_type
+
+  validates :grantmaker_id, :amount, :presence => true
   validates :fiscal_year, :length => { :is => 4 }
 
   belongs_to :grantmaker
   has_many :deadlines
-  
-  attr_accessible :amount, :ask_status, :fiscal_year, :funding_type, :grantmaker_id, :notes, :notification_date, :organization_id, :program, :request_type
 
   ASK_STATUSES = %w{submitting pending received rejected accepted}
   REQUEST_TYPES = ['loi', 'full proposal', 'thank you', '6 month report', 'final report']
@@ -20,7 +21,7 @@ class GrantRecord < ActiveRecord::Base
  def self.program
     return [AiLi, SK, FP]
   end
-  
+
   def ask_status_must_be_valid
     if ask_status.present? && ASK_STATUSES.exclude?(ask_status)
       errors.add(:ask_status, "must be one of the following: #{ASK_STATUSES.join(', ')}")
